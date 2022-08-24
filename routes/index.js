e1b41a49c3940fdf7b8aeb56fcdd09156c01ab01
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-08-24 19:41:56 -0300

    [feat] GET Sales endpoints
    Adding the endpoints to GET all sales and sales by id to the router index

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ const router = express.Router();
 
 router.get('/products', controllers.getAllProducts);
 router.get('/products/:id', controllers.getProductsById);
+router.get('/sales', controllers.getAllSales);
+router.get('/sales/:id', controllers.getSalesById);
+
 router.post('/products', nameValidation, controllers.postNewProduct);
 
 module.exports = router;
