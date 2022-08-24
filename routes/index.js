5c830278a64b414708a44106f4776a883675c0bd
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-08-24 18:02:29 -0300

    [feat] Adding POST method
    New line on routes index with POST method to create new product

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/products', controllers.getAllProducts);
 router.get('/products/:id', controllers.getProductsById);
+router.post('/products', controller.postProduct);
 
 module.exports = router;
