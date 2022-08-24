47ef20ae7fbe02cfa615b4dfd3751b4a3f1d6d62
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-08-24 11:53:44 -0300

    [feat] Configuring app and routes
    Adding app.use to express.json() and routes, and configuring the index.js on the routes folder

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
 const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
