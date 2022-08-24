c5dc9583e02b0fed828521d77fa00014c6859051
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-08-24 17:02:42 -0300

    [feat] Adding Products service
    Creating the service that itermediates the model result and controller request

---
+++ services/ProductsService.js
@@ -0,0 +1,15 @@
+const { getAll, getById } = require("../models/ProductsModel");
+
+const getProducts = async (id = null) => {
+  if (!id) {
+    const result = await getAll();
+    return result;
+  }
+  const result = await getById(id);
+  if (!result) return null;
+  return result;
+};
+
+module.exports = {
+  getProducts,
+};
