579a500804507e402c45892a306e7b317f360bbc
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-08-24 17:01:47 -0300

    [feat] Adding Products model
    Creating first model, using the database connection to SELECT the proper information as requested

---
+++ models/ProductsModel.js
@@ -0,0 +1,25 @@
+const connection = require('../db/connection');
+
+const getAll = async () => {
+  const [result] = await connection.query(`
+    SELECT * FROM
+      StoreManager.products
+  `);
+  return result;
+};
+
+const getById = async (id) => {
+  const [[result]] = await connection.query(`
+    SELECT * FROM
+      StoreManager.products
+    WHERE
+      id = ?
+  `, [id]);
+
+  return result;
+};
+
+module.exports = {
+  getAll,
+  getById,
+};
