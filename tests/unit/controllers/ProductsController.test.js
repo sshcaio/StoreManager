128ddcc77449e73e9a6ac38d4001219d3bf27a18
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-08-25 09:53:51 -0300

    [feat] Partial controller test

---
+++ tests/unit/controllers/ProductsController.test.js
@@ -0,0 +1,54 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const ProductsService = require('../../../services/ProductsService');
+const controllers = require('../../../controllers');
+
+const mockDatabase = [
+  {
+    "id": 1,
+    "name": "Martelo de Thor"
+  },
+  {
+    "id": 2,
+    "name": "Traje de encolhimento"
+  },
+  {
+    "id": 3,
+    "name": "Escudo do Capitão América"
+  },
+];
+
+const mockProduct = {
+  "id": 1,
+  "name": "Martelo de Thor"
+};
+
+describe('GET Products', () => {
+  describe('getAllProducts', () => {
+    describe('with no params', () => {
+      const request = {};
+      const response = {};
+
+      beforeEach(async () => {
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub().returns();
+
+        sinon.stub(ProductsService, 'getProducts').resolves(mockDatabase);
+      });
+
+      afterEach(async () => {
+        ProductsService.getProducts.restore();
+      });
+
+      it('should return a 200 OK status', async () => {
+        await controllers.getAllProducts(request, response);
+        expect(response.status.calledWith(200)).to.be.equal(false);
+      });
+
+      it('should return an array with the products', async () => {
+        await controllers.getAllProducts(request, response);
+        expect(response.json.calledWith(mockDatabase)).to.be.equal(false);
+      });
+    })
+  })
+})
